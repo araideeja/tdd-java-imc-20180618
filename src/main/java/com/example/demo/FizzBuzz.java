531c9978e6db315d0954fa3f9cd06f2f3fc1abd5
somkiat <somkiat.p@gmail.com>
2018-06-19 12:50:05 +0700

    Workshop with FizzBuzz

---
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class FizzBuzz {
+
+    public String say(int input) {
+        if(input == 15) 
+            return "FizzBuzz";
+        if(input%5 == 0) 
+            return "Buzz";
+        if(input%3 == 0)
+            return "Fizz";
+        return input + "";
+    }
+
+}
